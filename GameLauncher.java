087c38357266aafb75a6cd9384128bda5d97bbf7
MrHev <drhev@yandex.ru>
2018-09-05 12:04:03 +0300

    GuessGame

---
+++ GameLauncher.java
@@ -0,0 +1,9 @@
+/**
+ * Created by SkyNet on 05.09.2018.
+ */
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
---
+++ Player.java
@@ -0,0 +1,11 @@
+/**
+ * Created by SkyNet on 05.09.2018.
+ */
+public class Player {
+    int number = 0;
+
+    public void guess() {
+        number = (int) (Math.random() * 10);
+        System.out.println("Думаю, это число " + number);
+    }
+}
