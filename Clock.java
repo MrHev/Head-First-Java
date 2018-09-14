0bd30370655ac7c2b916955613233841ac858a4c
MrHev <drhev@yandex.ru>
2018-09-14 18:52:43 +0300

    p.118

---
+++ Clock.java
@@ -0,0 +1,22 @@
+/**
+ * Created by SkyNet on 14.09.2018.
+ */
+class Clock {
+    String time;
+
+    void setTime(String t) {
+        time = t;
+    }
+    String getTime() {
+        return time;
+    }
+}
+class ClockTestDrive {
+    public static void main(String[] args) {
+        Clock c = new Clock();
+
+        c.setTime("1245");
+        String tod = c.getTime();
+        System.out.println("время: " + tod);
+    }
+}
